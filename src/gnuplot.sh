f16f775d984173b0b0a9069ab3eccca26f303842
vetch <vetch97@gmail.com>
2019-06-03 10:28:12 +0200

    fixed just one simple typo

--- src/gnuplot.sh
+++ src/gnuplot.sh
@@ -4,5 +4,5 @@ do
     done
 for i in timesignal afterNoise afterCorrection
 do
-    gnuplot -e    "set terminal png size 1200,800;    set xlabel 'Time';    set ylabel 'Valu';    set output 'res/plot_$i.png';    plot 'res/$i.dat' with lines";
+    gnuplot -e    "set terminal png size 1200,800;    set xlabel 'Time';    set ylabel 'Value';    set output 'res/plot_$i.png';    plot 'res/$i.dat' with lines";
 done
